3b1dd79386409e960684e93fa836082586bdabf1
nitturisrinu <nsrinu540@gmail.com>
2022-01-15 00:18:51 +0530

    CHANGED FILE UPDATED

---
+++ src/srinujava/DemoApp.java
@@ -0,0 +1,28 @@
+package srinujava;
+
+public class DemoApp {
+	
+	/*
+	 * void m1(String a) { System.out.println("hiikk");
+	 * 
+	 * }
+	 */
+	void m1(int  a)
+	{
+		System.out.println("hiikk");
+		
+	}
+	
+	void m1(int a,int b)
+	{
+		System.out.println("hii");
+		
+	}
+	public static void main(String[] args) {
+		DemoApp d=new DemoApp();
+		d.m1('a');
+		
+	}
+	
+	
+}
